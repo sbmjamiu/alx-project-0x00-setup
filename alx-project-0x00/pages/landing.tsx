e48cff7e371a8112a0e28b198e4f62f9d2775b33
Jamiu Abdulhakeem <sbmjamiu@gmail.com>
2025-05-28 11:00:46 +0100

    add button to the landing page

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,3 +1,4 @@
+import Button from "@/components/Button";
 import Card from "@/components/Card";
 
 const Landing: React.FC = () => {
@@ -5,6 +6,27 @@ const Landing: React.FC = () => {
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
       <Card />
+
+      {/* Small Buttons */}
+      <div className="flex gap-4 mb-2">
+        <Button title="Small - sm" styles="text-sm rounded-sm" />
+        <Button title="Small - md" styles="text-sm rounded-md" />
+        <Button title="Small - full" styles="text-sm rounded-full" />
+      </div>
+
+      {/* Medium Buttons */}
+      <div className="flex gap-4 mb-2">
+        <Button title="Medium - sm" styles="text-base rounded-sm" />
+        <Button title="Medium - md" styles="text-base rounded-md" />
+        <Button title="Medium - full" styles="text-base rounded-full" />
+      </div>
+
+      {/* Large Buttons */}
+      <div className="flex gap-4 mb-2">
+        <Button title="Large - sm" styles="text-lg rounded-sm" />
+        <Button title="Large - md" styles="text-lg rounded-md" />
+        <Button title="Large - full" styles="text-lg rounded-full" />
+      </div>
     </div>
   );
 };
