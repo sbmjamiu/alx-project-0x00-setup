53241dbe8b2565dca646555a508bf21eb4935470
Jamiu Abdulhakeem <sbmjamiu@gmail.com>
2025-05-28 10:59:37 +0100

    Button components created

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,13 @@
+import { ButtonProps } from "@/interfaces";
+
+const Button: React.FC<ButtonProps> = ({ title, styles }) => {
+  return (
+    <button
+      className={`px-4 py-2 font-medium text-white bg-blue-600 hover:bg-blue-700 transition ${styles}`}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default Button;
